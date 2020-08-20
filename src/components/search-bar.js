6d4460f837ff9d7f7b8faf42570226866fd35776
James Sturges <james.sturges@slalom.com>
2020-08-20 15:10:55 -0400

    fixed menu style

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -37,16 +37,27 @@ class SearchBar extends Component {
   render() {
     const { alertMessage } = this.state;
 
-    const alertClass = `search-alert opacity-${alertMessage ? '1' : '0'}`;
+    const menuStyle = {
+      borderRadius: '3px',
+      boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
+      background: 'rgba(255, 255, 255, 0.9)',
+      padding: '2px 0',
+      fontSize: '90%',
+      position: 'fixed',
+      overflow: 'auto',
+      maxHeight: '50%', // TODO: don't cheat, let it flow to the bottom
+      zIndex: '998'
+    };
 
     return (
-      <div className="SearchBar clearfix">
+      <div className="SearchBar">
         <form
           className="input-group search-form"
           onSubmit={this.onFormSubmit.bind(this)}>
           <Autocomplete
             getItemValue={item => item}
             items={moment.tz.names()}
+            menuStyle={menuStyle}
             shouldItemRender={(item, value) => {
               return item.toLowerCase().includes(value.toLowerCase());
             }}
@@ -68,7 +79,7 @@ class SearchBar extends Component {
             <button type="submit" className="btn btn-primary">Add</button>
           </div>
         </form>
-        <div className={alertClass}>{alertMessage}</div>
+        {alertMessage && <div className="search-alert opacity-1">{alertMessage}</div>}
       </div>
     );
   }
